b9ddc5a8b6846b61c027e57ed4a9b090d37bee50
Lefteris Psitopoulos <lefteris.psitopoulos@thanpa.com>
2017-05-30 12:53:21 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+jQuery('document').ready( function($) {
+    $('.gallery-filters li').on('click', function() {
+        $('.gallery-filters li').removeClass('active');
+        $(this).addClass('active');
+        filter = $(this).attr('gallery-filter');
+
+        $('.gallery-img-holder').each( function() {
+            if (filter == 'all') {
+                $(this).fadeIn(); 
+            } else {
+                $(this).hide();
+                if ($(this).attr('filter-category') == filter) {
+                    $(this).fadeIn();
+                }
+            }
+        });
+    });
+});
