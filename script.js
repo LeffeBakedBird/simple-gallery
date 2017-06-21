aa74408662c7e57d1d5bcac0997eeaadab3d5f77
Lefteris Psitopoulos <lefteris.psitopoulos@thanpa.com>
2017-06-21 16:54:01 +0300

    Added lightbox. Created settings panel

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 jQuery('document').ready( function($) {
+
+    // settings panel
+    $('.toggler').on('click', function() {
+        $(this).parents('.config-settings').toggleClass('open');
+    });
+
+    $('.lb-setting').on('click', function() {
+        $(this).toggleClass('disabled');
+
+        if ($(this).hasClass('disabled')) {
+            $(this).children('.glyphicon').addClass('glyphicon-remove-circle');
+            $(this).children('.glyphicon').removeClass('glyphicon-ok-circle');
+            $('.lightbox').addClass('disabled');
+        } else {
+            $(this).children('.glyphicon').removeClass('glyphicon-remove-circle');
+            $(this).children('.glyphicon').addClass('glyphicon-ok-circle');
+            $('.lightbox').removeClass('disabled');
+        }
+    });
+
+    // the gallery filters
     $('.gallery-filters li').on('click', function() {
         $('.gallery-filters li').removeClass('active');
         $(this).addClass('active');
@@ -14,5 +35,22 @@ jQuery('document').ready( function($) {
                 }
             }
         });
+    });
+
+    // the lightbox functionality
+    $('.gallery-img-holder').on('click', function() {
+        if ($('.lightbox').hasClass('disabled')) {
+            return;
+        }
+        var ClickedImgSrc = $(this).children('img').attr('src');
+        var theLightboxImage = $('.lightbox>.img-container>img'); 
+        theLightboxImage.attr('src', ClickedImgSrc);
+        $('.lightbox .close-btn').css('left', theLightboxImage.width()/2);
+        $('.lightbox').addClass('open');
+    });
+
+    // close the lightbox
+    $('.lightbox .close-btn').on('click', function() {
+        $('.lightbox').removeClass('open');
     });
 });
